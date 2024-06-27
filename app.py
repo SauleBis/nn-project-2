78cbf99b52ee48f2fb819d93a8d39e1160b9be47
SauleBis <saule.bisengalieva@gmail.com>
2024-06-27 13:25:41 -0400

    update app

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
-import torch
-import torchvision
-import torch.nn as nn
-from torch.utils.data import DataLoader, random_split
-from torchvision import datasets
-from torchvision import transforms as T
-from torchvision import io
-import torchutils as tu
-import json
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import pickle as pkl
-from torchvision.models import resnet18
 import streamlit as st
-from PIL import Image
+
 
 
 
